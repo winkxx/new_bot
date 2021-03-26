96ee22b955b4ae1cb7daa968a011a9c785fc90c7
666wcy <769020367@qq.com>
2021-03-26 14:38:50 +0800

    Update video.py

--- bot/modules/video.py
+++ bot/modules/video.py
@@ -2,22 +2,30 @@
 import threading
 import youtube_dl
 from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton
+import sys
 
 def get_video_info(client, message, url):
-    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
-    result = ydl.extract_info(
-        url=url,
-        download=False,
+    try:
+        ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
+        result = ydl.extract_info(
+            url=url,
+            download=False,
 
-    )
-    print(result)
-    video_name=result['title']
-    video_description=result['description']
-    video_img=result['thumbnails'][len(result['thumbnails'])-1]["url"]
-    video_uploader=result['uploader']
-    text=f"视频名称：{video_name}\n" \
-         f"作者:{video_uploader}\n" \
-         f"简介：{video_description}\n"
+        )
+        #print(result)
+        video_name=result['title']
+        video_description=result['description']
+        video_img=result['thumbnails'][len(result['thumbnails'])-1]["url"]
+        video_uploader=result['uploader']
+        text=f"视频名称：{video_name}\n" \
+             f"作者:{video_uploader}\n" \
+             f"简介：{video_description}\n"
+        print(text)
+        print(video_img)
+        sys.stdout.flush()
+    except Exception as e:
+        client.send_message(chat_id=message.chat.id, text=f"无法获取视频信息:\n{e}", parse_mode='markdown')
+
     new_inline_keyboard = [
         [
             InlineKeyboardButton(
