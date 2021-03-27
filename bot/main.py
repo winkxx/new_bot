0ccf28b53ebab5e4b5441703e8234eb479f1b02e
666wcy <769020367@qq.com>
2021-03-27 22:19:43 +0800

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -28,11 +28,50 @@ async def chexk_group(_, client, query):
 
 
 
-async def test(client, message):
+async def help(client, message):
     print(client)
     print(message)
-    message.reply_text(message.text)
-    await client.send_message(chat_id=int(Telegram_user_id), text="test")
+    text='''********** pixiv相关 **********
+/pixivuser - 打包下载P站画师的全部作品上传至云盘
+/pixivusertg - 压缩打包下载P站画师的全部作品发送到TG
+/pixivuserphoto - 下载P站画师的全部作品图片方式发送
+在命令后加入作品ID，使用示例
+/pixivuser 9675329
+
+/pixivpid - 发送pixiv该id的图片
+在命令后加入作品ID，使用示例
+/pixivpid 88339301
+
+********** aria2相关 **********
+/magfile - 推送种子文件至aria2下载
+/mirror - 推送直链至aria2下载上传至网盘
+/mirrortg - 推送直链至aria2下载发送到TG
+/magnet - 推送磁力链接至aria2下载
+以上在命令后加入链接
+
+********** 其它相关 **********
+/downtgfile - 发送TG文件并上传至网盘
+发送 /downtgfile 后按提示发送文件即可
+
+/search - 在哔咔搜索本子
+示例 /search 本子名
+
+/rclonecopyurl - 用rclonecopyurl的方式直接上传直链文件
+示例 /rclonecopyurl 文件直链
+
+/getfileid - 发送文件获取fileid
+发送 /getfileid  后按提示发送文件即可
+
+/getfile - 发送fileid来获取文件
+示例 /getfile  文件直链
+
+/video - 使用youtube-dl下载视频
+示例 /getfile  视频链接
+目前测试youtube和哔哩哔哩(不包括番剧)完美适配
+
+Bot相关联系：https://t.me/Ben_chao'''
+    message.reply_text(text)
+    
 
 def start_bot():
     #scheduler = BlockingScheduler()
@@ -52,9 +91,9 @@ def start_bot():
 
 
     start_message_handler = MessageHandler(
-        test,
+        help,
         #filters=filters.command("start") & filters.user(int(Telegram_user_id))
-        filters=filters.command("start") & filters.create(chexk_group)
+        filters=filters.command("help") 
     )
 
     pixivuser_message_handler = MessageHandler(
