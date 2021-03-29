2f992d26ff5fbe57117eecf3acdf024f4690fa39
666wcy <769020367@qq.com>
2021-03-29 21:06:57 +0800

    new

--- bot/web.py
+++ bot/web.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request,send_file
 from requests import get,post
 
 import os
@@ -21,50 +21,7 @@ def proxypost():
 
 @app.route('/', methods=['GET'])
 def index():
-    text = '''
-    唤醒成功！！！(也许)
-    
-    ********** pixiv相关 **********
-/pixivuser - 打包下载P站画师的全部作品上传至云盘
-/pixivusertg - 压缩打包下载P站画师的全部作品发送到TG
-/pixivuserphoto - 下载P站画师的全部作品图片方式发送
-pixivusertele - 下载P站画师的全部作品网页方式发送
-在命令后加入作品ID，使用示例
-/pixivuser 9675329
-
-/pixivpid - 发送pixiv该id的图片
-在命令后加入作品ID，使用示例
-/pixivpid 88339301
-
-********** aria2相关 **********
-/magfile - 推送种子文件至aria2下载
-/mirror - 推送直链至aria2下载上传至网盘
-/mirrortg - 推送直链至aria2下载发送到TG
-/magnet - 推送磁力链接至aria2下载
-以上在命令后加入链接
-
-********** 其它相关 **********
-/downtgfile - 发送TG文件并上传至网盘
-发送 /downtgfile 后按提示发送文件即可
-
-/search - 在哔咔搜索本子
-示例 /search 本子名
-
-/rclonecopyurl - 用rclonecopyurl的方式直接上传直链文件
-示例 /rclonecopyurl 文件直链
-
-/getfileid - 发送文件获取fileid
-发送 /getfileid  后按提示发送文件即可
-
-/getfile - 发送fileid来获取文件
-示例 /getfile  文件直链
-
-/video - 使用youtube-dl下载视频
-示例 /getfile  视频链接
-目前测试youtube和哔哩哔哩(不包括番剧)完美适配
-
-Bot相关联系：https://t.me/Ben_chao'''
-    return text, 200
+    return send_file("index.html")
 
 @app.route('/jsonrpc/',methods=['GET'])
 def proxyget():
