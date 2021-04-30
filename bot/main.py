73855a91efe338228ba68e1edd27fee935cff0a9
winkxx <68945988+winkxx@users.noreply.github.com>
2021-04-30 18:46:10 +0800

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -12,6 +12,11 @@ from modules.moretg import get_telegram_file,get_file_id,sendfile_by_id
 from modules.picacg import seach_main
 from modules.rclone import start_rclonecopy,start_rclonelsd,start_rclonels,start_rclonecopyurl
 from modules.video import start_get_video_info
+import hashlib
+import os
+#import md5 #Python2里的引用
+
+
 
 async def chexk_group(_, client, query):
     print(query)
@@ -33,7 +38,7 @@ async def test(client, message):
     await client.send_message(chat_id=int(Telegram_user_id), text="test")
 
 def start_bot():
-    scheduler = BlockingScheduler()
+    #scheduler = BlockingScheduler()
     scheduler = BackgroundScheduler()
 
     scheduler.add_job(new_clock, "interval", seconds=60)
@@ -51,7 +56,7 @@ def start_bot():
 
     start_message_handler = MessageHandler(
         test,
-        filters=filters.command("start") & filters.user(int(Telegram_user_id))
+        #filters=filters.command("start") & filters.user(int(Telegram_user_id))
         filters=filters.command("start") & filters.create(chexk_group)
     )
 
